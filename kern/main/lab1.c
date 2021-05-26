99f66aac271cd5554f5f492922b604512f24e9be
grace <grace@pop-os.localdomain>
2021-05-26 11:42:35 +0200

    first commit

---
+++ kern/main/lab1.c
@@ -0,0 +1,27 @@
+
+#include <types.h>
+#include <kern/errno.h>
+#include <kern/reboot.h>
+#include <kern/unistd.h>
+#include <lib.h>
+#include <spl.h>
+#include <clock.h>
+#include <thread.h>
+#include <proc.h>
+#include <current.h>
+#include <synch.h>
+#include <vm.h>
+#include <mainbus.h>
+#include <vfs.h>
+#include <device.h>
+#include <syscall.h>
+#include <test.h>
+#include <version.h>
+#include "autoconf.h"  // for pseudoconfig
+
+#include "lab1.h"
+void lab1 (void) {
+  kprintf("HELLO OS161\n");
+}
+
+
